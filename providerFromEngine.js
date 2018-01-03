c7eed4ad8f160e94b46cc0c3e7472a3ccc209712
kumavis <aaron@kumavis.me>
2018-01-02 16:58:01 -0800

    providerFromEngine - server-push s1upport

--- providerFromEngine.js
+++ providerFromEngine.js
@@ -1,6 +1,11 @@
+const EventEmitter = require('events')
+
 module.exports = providerFromEngine
 
 function providerFromEngine (engine) {
-  const provider = { sendAsync: engine.handle.bind(engine) }
+  const provider = new EventEmitter()
+  provider.sendAsync = engine.handle.bind(engine)
+  // rebroadcast the 'data' event from engine to provider
+  engine.on('data', provider.emit.bind(provider, 'data'))
   return provider
 }
