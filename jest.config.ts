df6b48d9e87670beb05016c7ac170fee5c916eca
Elliot Winkler <elliot.winkler@gmail.com>
2022-04-27 14:15:03 -0600

    Exclude types.ts from the coverage report

--- jest.config.ts
+++ jest.config.ts
@@ -30,9 +30,7 @@ const config: Config.InitialOptions = {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: ['\\/src\\/types\\.ts$'],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
