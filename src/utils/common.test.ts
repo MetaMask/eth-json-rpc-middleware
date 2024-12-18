45b115ba71bb6d19762b895c1622c751c0d6db04
jpuri <jyotipuri@gmail.com>
2024-12-18 20:03:20 +0530

    update

--- src/utils/common.test.ts
+++ src/utils/common.test.ts
@@ -4,11 +4,11 @@ describe('CommonUtils', () => {
   describe('stripArrayTypeIfPresent', () => {
     it('remove array brackets from the type if present', () => {
       expect(stripArrayTypeIfPresent('string[]')).toBe('string');
-      expect(stripArrayTypeIfPresent('string []')).toBe('string');
     });
 
     it('return types which are not array without any change', () => {
       expect(stripArrayTypeIfPresent('string')).toBe('string');
+      expect(stripArrayTypeIfPresent('string []')).toBe('string []');
     });
   });
 });
