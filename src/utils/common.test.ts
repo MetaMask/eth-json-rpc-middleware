5fdeecdd9fa78df8baaacec66f925f9ba3f59caa
jpuri <jyotipuri@gmail.com>
2024-12-19 20:34:05 +0530

    update

--- src/utils/common.test.ts
+++ src/utils/common.test.ts
@@ -10,6 +10,7 @@ describe('CommonUtils', () => {
     it('return types which are not array without any change', () => {
       expect(stripArrayTypeIfPresent('string')).toBe('string');
       expect(stripArrayTypeIfPresent('string []')).toBe('string []');
+      expect(stripArrayTypeIfPresent(undefined as unknown as string)).toBe(undefined);
     });
   });
 });
