6f4e30d37af87ec29323ba7b144c59c64239b3c6
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2025-10-10 15:54:12 -0700

    chore: eslint --fix

--- src/retryOnEmpty.test.ts
+++ src/retryOnEmpty.test.ts
@@ -21,27 +21,33 @@ import {
 
 /**
  * Objects used in each test.
- *
- * @property engine - The engine that holds the middleware stack, including the
- * one being tested.
- * @property provider - The provider that is used to make requests against
- * (which the middleware being tested will react to).
- * @property blockTracker - The block tracker which is used inside of the
- * middleware being tested.
  */
 interface Setup {
+  /**
+   * The engine that holds the middleware stack, including the
+   * one being tested.
+   */
   engine: JsonRpcEngine;
+  /**
+   * The provider that is used to make requests against
+   * (which the middleware being tested will react to).
+   */
   provider: SafeEventEmitterProvider;
+  /**
+   * The block tracker which is used inside of the
+   * middleware being tested.
+   */
   blockTracker: PollingBlockTracker;
 }
 
 /**
  * Options supported by `withTestSetup`.
- *
- * @property configureMiddleware - A function which determines which middleware
- * should be added to the engine.
  */
 interface WithTestSetupOptions {
+  /**
+   * A function which determines which middleware
+   * should be added to the engine.
+   */
   configureMiddleware: (setup: Setup) => {
     middlewareUnderTest: JsonRpcMiddleware<any, any>;
     otherMiddleware?: JsonRpcMiddleware<any, any>[];
@@ -57,15 +63,9 @@ interface WithTestSetupOptions {
  */
 type WithTestSetupCallback<T> = (setup: Setup) => Promise<T>;
 
-const originalSetTimeout = setTimeout;
-
 describe('createRetryOnEmptyMiddleware', () => {
   beforeEach(() => {
-    jest.useFakeTimers();
-  });
-
-  afterEach(() => {
-    jest.useRealTimers();
+    jest.clearAllTimers();
   });
 
   it('throws if not given a provider', async () => {
@@ -775,7 +775,7 @@ async function waitForRequestToBeRetried({
   let iterationNumber = 1;
 
   while (iterationNumber <= numberOfTimes) {
-    await new Promise((resolve) => originalSetTimeout(resolve, 0));
+    await new Promise((resolve) => process.nextTick(resolve));
 
     if (
       requestSpy.mock.calls.filter((args) => requestMatches(args[0], request))
--- src/utils/timeout.test.ts
+++ src/utils/timeout.test.ts
@@ -1,59 +1,67 @@
-import { nextTick } from 'process';
-
 import { timeout } from './timeout';
 
 describe('timeout', () => {
-  afterEach(() => {
-    jest.useRealTimers();
+  describe('with real timers', () => {
+    beforeAll(() => {
+      jest.useRealTimers();
+    });
+
+    afterAll(() => {
+      jest.useFakeTimers();
+    });
+
+    it('does not resolve in the current event loop', async () => {
+      const winner = await Promise.race([
+        (async () => {
+          await timeout(0);
+          return 'timeout';
+        })(),
+        (async () => {
+          // nextTick runs immediately at the start of the next event loop
+          await new Promise((resolve) => process.nextTick(resolve));
+          return 'nextTick';
+        })(),
+      ]);
+
+      expect(winner).toBe('nextTick');
+    });
+
+    it('resolves in the next event loop', async () => {
+      const winner = await Promise.race([
+        (async () => {
+          await timeout(0);
+          return 'timeout';
+        })(),
+        (async () => {
+          // setImmediate will run all queued functions
+          await new Promise((resolve) => setImmediate(resolve));
+          return 'setImmediate';
+        })(),
+      ]);
+
+      expect(winner).toBe('setImmediate');
+    });
   });
 
-  it('does not resolve in the current event loop', async () => {
-    const winner = await Promise.race([
-      (async () => {
-        await timeout(0);
-        return 'timeout';
-      })(),
-      (async () => {
-        // nextTick runs immediately at the start of the next event loop
-        await new Promise((resolve) => nextTick(resolve));
-        return 'nextTick';
-      })(),
-    ]);
+  describe('with fake timers', () => {
+    afterEach(() => {
+      jest.clearAllTimers();
+    });
 
-    expect(winner).toBe('nextTick');
-  });
+    it('does not resolve before given duration', async () => {
+      const promise = timeout(100);
 
-  it('resolves in the next event loop', async () => {
-    const winner = await Promise.race([
-      (async () => {
-        await timeout(0);
-        return 'timeout';
-      })(),
-      (async () => {
-        // setImmediate will run all queued functions
-        await new Promise((resolve) => setImmediate(resolve));
-        return 'setImmediate';
-      })(),
-    ]);
+      jest.advanceTimersByTime(50);
 
-    expect(winner).toBe('setImmediate');
-  });
+      expect(promise).toNeverResolve();
+    });
 
-  it('does not resolve before given duration', async () => {
-    jest.useFakeTimers();
-    const promise = timeout(100);
+    it('resolves after the given duration', async () => {
+      const promise = timeout(100);
 
-    jest.advanceTimersByTime(50);
+      jest.advanceTimersByTime(100);
 
-    expect(promise).toNeverResolve();
-  });
-
-  it('resolves after the given duration', async () => {
-    jest.useFakeTimers();
-    const promise = timeout(100);
-
-    jest.advanceTimersByTime(100);
-
-    expect(await promise).toBeUndefined();
+      expect(await promise).toBeUndefined();
+    });
   });
 });
